45ddb4f744d9848c27d3fd83cff1c81ac99d86ba
Peter Schilder <petercschilder@lewisu.edu>
2016-01-21 22:43:05 -0600

    Class 3
    Added Next button and multiple questions

---
+++ QuizApp/app/src/main/java/edu/lewisu/cs/peterschilder/quizapp/Question.java
@@ -0,0 +1,30 @@
+package edu.lewisu.cs.peterschilder.quizapp;
+
+/**
+ * Created by Peter Schilder on 1/21/2016.
+ */
+public class Question {
+    int textResId;
+    boolean answerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        this.textResId = textResId;
+        this.answerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return textResId;
+    }
+
+    public void setTextResId(int textResId) {
+        this.textResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return answerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.answerTrue = answerTrue;
+    }
+}
--- QuizApp/app/src/main/java/edu/lewisu/cs/peterschilder/quizapp/QuizActivity.java
+++ QuizApp/app/src/main/java/edu/lewisu/cs/peterschilder/quizapp/QuizActivity.java
@@ -4,40 +4,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
     private Button trueButton;
     private Button falseButton;
+    private Button nextButton;
+    private TextView questionTextView;
+
+    private Question[] questions = new Question[]{
+            new Question(R.string.question1,true),
+            new Question(R.string.question2,false),
+            new Question(R.string.question3,true),
+            new Question(R.string.question4,false)
+    };
+
+    private int currentIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
         trueButton = (Button)findViewById(R.id.true_button);
         falseButton = (Button)findViewById(R.id.false_button);
 
-        trueButton.setOnClickListener(new CorrectClickListener());
-        falseButton.setOnClickListener(new IncorrectClickListener());
+        trueButton.setOnClickListener(new TrueClickListener());
+        falseButton.setOnClickListener(new FalseClickListener());
+
+        nextButton = (Button)findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new NextClickListener());
+
+        questionTextView = (TextView)findViewById(R.id.display_question);
+
+        updateQuestion();
 
     }
 
-    private class CorrectClickListener implements View.OnClickListener{
+    private void updateQuestion(){
+        int question = questions[currentIndex].getTextResId();
+        questionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPress){
+        boolean correctAnswer = questions[currentIndex].isAnswerTrue();
+
+        int toastTextID;
+
+        if(userPress == correctAnswer){
+            toastTextID = R.string.correct_toast;
+        }
+        else {
+            toastTextID = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, toastTextID, Toast.LENGTH_SHORT).show();
+    }
+
+    private class TrueClickListener implements View.OnClickListener{
         @Override
         public void onClick(View view){
-            Toast.makeText(QuizActivity.this,
-                    R.string.correct_toast,
-                    Toast.LENGTH_SHORT).show();
+           checkAnswer(true);
 
         }
     }
-    private class IncorrectClickListener implements View.OnClickListener{
+    private class FalseClickListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            Toast.makeText(QuizActivity.this,
-                    R.string.incorrect_toast,
-                    Toast.LENGTH_SHORT).show();
-
+           checkAnswer(false);
         }
     }
+
+    private class NextClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            currentIndex = (currentIndex + 1) % questions.length;
+            updateQuestion();
+        }
+    }
+
 }
