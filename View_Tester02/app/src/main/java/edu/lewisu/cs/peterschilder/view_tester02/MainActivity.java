d915426a5313f07c8014092ec77825eab64460f8
Peter Schilder <petercschilder@lewisu.edu>
2016-02-06 13:24:59 -0600

    VT_02
    The stars are invisible for some reason.

--- View_Tester02/app/src/main/java/edu/lewisu/cs/peterschilder/view_tester02/MainActivity.java
+++ View_Tester02/app/src/main/java/edu/lewisu/cs/peterschilder/view_tester02/MainActivity.java
@@ -2,12 +2,16 @@ package edu.lewisu.cs.peterschilder.view_tester02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.RatingBar;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private RatingBar ratingBar;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +19,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ratingBar = (RatingBar)findViewById(R.id.ratingBar);
         ratingBar.setOnRatingBarChangeListener(new BarListener());
-
+        spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(new SpinListener());
     }
 
     private class BarListener implements RatingBar.OnRatingBarChangeListener{
         @Override
         public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
             Toast.makeText(getApplicationContext(), rating + " stars", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private class SpinListener implements AdapterView.OnItemSelectedListener{
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String selection = (String)parent.getItemAtPosition(position);
+            Toast.makeText(getApplicationContext(), selection,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
         }
     }
 }
