41a0fbe8bb14bc65ffdd169f89ab5891e496054f
Peter Schilder <petercschilder@lewisu.edu>
2016-04-22 19:51:09 -0500

    LocationLab

--- LocationLab/app/src/main/java/edu/lewisu/cs/peterschilder/locationlab/MainActivity.java
+++ LocationLab/app/src/main/java/edu/lewisu/cs/peterschilder/locationlab/MainActivity.java
@@ -1,13 +1,77 @@
 package edu.lewisu.cs.peterschilder.locationlab;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener, LocationListener{
+    public TextView textOutput;
+    private GoogleApiClient googleApiClient;
+    private static final String TAG = "location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textOutput = (TextView)findViewById(R.id.textOutput);
+
+        GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
+        builder.addApi(LocationServices.API);
+        builder.addConnectionCallbacks(this);
+        builder.addOnConnectionFailedListener(this);
+        googleApiClient = builder.build();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(1000);
+
+        try{
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+        }catch (SecurityException e){
+            Log.w(TAG, e.getMessage());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connections Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection Failed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "Location Changed");
+        textOutput.setText(Double.toString(location.getLatitude()));
+        
     }
 }
